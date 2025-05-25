0446691e6cccd4f2228620af4339992fad2c8ed2
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-05-25 22:17:15 +0530

    Allotment css issue fixed

--- assets/YadlEditor.tsx
+++ assets/YadlEditor.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
+// import "@xyflow/react/dist/style.css";
+// import "yadl-preview/dist/yadl-preview.css";
+// import "react-fontpicker-ts/dist/index.css";
+import { Allotment } from "allotment";
 
-export default function YadlEditor() {
+export default function YadlEditorContainer() {
+
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr" }}>
-      <div style={{ minHeight: "74vh" }}>
-        <h2>From React App</h2>
+    <div style={{ display: "grid", gridTemplateColumns: "1fr" }} data-theme={"dark"}>
+      <div style={{ minHeight: "96vh" }}>
+        <Allotment
+          snap
+        // defaultSizes={[100, 100, 100]}
+        >
+          <Allotment.Pane>
+            <div style={{ height: "100vh" }}>
+              <h1>Editor</h1>
+            </div>
+          </Allotment.Pane>
+          <Allotment.Pane>
+            <div style={{ height: "100vh" }}>
+              <h1>Preview</h1>
+            </div>
+          </Allotment.Pane>
+          <Allotment.Pane>
+            <h1>Search</h1>
+          </Allotment.Pane>
+        </Allotment>
       </div>
     </div>
   );
@@ -13,4 +35,4 @@ export default function YadlEditor() {
 
 
 const root = createRoot(document.getElementById('editor'));
-root.render(<YadlEditor />);
+root.render(<YadlEditorContainer />);
