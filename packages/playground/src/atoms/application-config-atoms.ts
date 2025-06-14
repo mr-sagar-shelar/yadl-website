fb3754b78abae5608bcacfd86df1ed368f9c346c
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-06-14 15:36:13 +0530

    Removed playground packages

--- packages/playground/src/atoms/application-config-atoms.ts
+++
@@ -1,9 +0,0 @@
-import { atomWithStorage } from 'jotai/utils';
-
-export const codeVisible = atomWithStorage("app-code-visible", true);
-export const searchShapesVisible = atomWithStorage("app-search-shapes-visible", true);
-
-export const previewTheme = atomWithStorage("app-preview-theme", {
-    value: "light",
-    title: "Light"
-});
